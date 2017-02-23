990c1227591614e3d63e9ec7d4eca66c2ed8e70c
roman1970 <r0man4ernyshev@gmail.com>
2017-02-23 16:41:15 +0700

    fist commit

--- config/params.php
+++ config/params.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'log_file' =>  '/var/log/nginx/access.log',
+    'email_host' => 'smtp-20.1gb.ru',
+    'cookieValidationKey' => 'Xu4Whj-Oq2yC2ANAE_iAWhYdr0w0K3Ei',
+    'mail_username' => 'u424229',
+    'mail_password' => '280699b4gh',
+    'api_key' => 'e44',
+    'db_host' => 'localhost',
+    'db_name' => 'sevens',
+    'db_username' => 'root',
+    'db_password' => 'vbifcdtnf'
+
 ];
