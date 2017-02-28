a21e4e39a1bd7b60cab58a00acddff4e9061fdc3
roman1970 <r0man4ernyshev@gmail.com>
2017-02-28 10:08:17 +0700

    radio on

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,13 +27,13 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Комната с мехом',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
+    /*echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
@@ -53,6 +53,7 @@ AppAsset::register($this);
             )
         ],
     ]);
+    */
     NavBar::end();
     ?>
 
@@ -60,15 +61,26 @@ AppAsset::register($this);
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
+
+
+
+
+            <div class="container">
+                <p class="text-center"><img src='<?=\yii\helpers\Url::to('/img/barded.png')?>' width="200px"></p>
+                <h1 class="text-center">РАДИО-БЛОГ <br>"Комната с мехом"</h1>
+                <p class="text-center">Ведущий - "Бард, который перевернул ЗИЛ" - Роман Беляшов!</p>
+            </div>
+            <audio id="au" autoplay >
+            </audio>
+
         <?= $content ?>
     </div>
 </div>
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; "Комната с мехом" <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
@@ -76,3 +88,95 @@ AppAsset::register($this);
 </body>
 </html>
 <?php $this->endPage() ?>
+<script>
+    $(document).ready(function() {
+
+        var au = document.getElementById('au');
+        au.src = 'http://37.192.187.83:10088/ices';
+
+
+        au.onerror = function () {
+            window.location = '/radio-off/';
+        };
+    });
+        
+        /*
+        
+
+        var name = $("#comm_name");
+        var body = $("#comm_body");
+        var content_id = $("#comm_cont");
+        var name_string = '';
+        var n = '';
+
+
+        var date = new Date();
+        $.cookie.raw = true;
+
+
+        $("#send").click(
+            function() {
+                if($.cookie('name')) name_string = $.cookie('name');
+                else name_string = name.val();
+
+                addComment(name_string, body.val());
+                sleep(2000);
+
+                $("#comments").load("/krokodile/default/callcomm/");
+                $('#w0').trigger( 'reset' ); //очищаем форму
+                $.cookie('name', name_string, { expires: 7 });
+
+                //setCookie('name', name_string, { expires: 7 });
+
+            });
+
+    });
+    function addComment(name, body, content) {
+
+        //Валидация
+        if (name === "") {
+            alert("Введите Имя или перезагрузите страницу");
+            return false;
+        }
+
+        if (body === "") {
+            alert("Введите Текст");
+            return false;
+        }
+
+
+        $.ajax({
+            type: "GET",
+            url: "/krokodile/default/addcomment/",
+            data: "name="+name+"&body="+body+"&content_id="+content,
+            success: function(html){
+                $("#base").html(html);
+            }
+
+        });
+
+    }
+    /*Симуляция сна
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+                break;
+            }
+        }
+    }
+
+    //Вытягиваем случайный айтем
+    setInterval(function () {
+
+        var script = document.createElement('script');
+
+        script.src = 'http://football-match.xyz/krokodile/default/rand-item/';
+        script.type = 'text/javascript';
+
+        document.body.appendChild(script);
+
+    }, 20000);
+        */
+
+</script>
