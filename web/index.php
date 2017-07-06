cd5933cfe2277daa93672c64d8cf47364b97184c
roman1970 <r0man4ernyshev@gmail.com>
2017-07-06 08:54:33 +0700

    regular

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo '<h1 style="text-align: center; padding-top: 50px;">АНТРАКТ, НЕГОДЯИ!</h1>'; exit;
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
