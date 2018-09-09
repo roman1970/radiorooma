8a66d3098783b42bfb7ee9165446b368923459e6
roman1970 <r0man4ernyshev@gmail.com>
2018-09-09 16:57:46 +0700

    fix come in

--- views/layouts/test.php
+++ views/layouts/test.php
@@ -12,6 +12,16 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-80881607-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-80881607-1');
+    </script>
+
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
