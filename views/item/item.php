081b0ab599ea8a28280bffb3c8ac221ade1b82fb
roman1970 <r0man4ernyshev@gmail.com>
2018-10-02 16:40:32 +0700

    likes del

--- views/item/item.php
+++ views/item/item.php
@@ -8,8 +8,8 @@
 
     <p id="on_button"><button type="button" class="btn btn-success" onclick="onAudio('<?=$item->audio?>')" >Воспроизвести</button></p>
     <p style="display: none" id="off_button"><button type="button" class="btn btn-success" onclick="offAudio()" >Переключиться на радио</button></p>
-    <p id="like_button"><button type="button" class="btn btn-success" onclick="like('<?=$item->id?>')" >Понравилось</button></p>
-    <?php //<p><audio controls src="http://37.192.187.83:10080/<?=$item->audio"></audio></p> ?>
+    <?php /*<p id="like_button"><button type="button" class="btn btn-success" onclick="like('<?=$item->id?>')" >Понравилось</button></p>
+    <p><audio controls src="http://37.192.187.83:10080/<?=$item->audio"></audio></p> */?>
     <p id="summary">Понравилось: <?=$item->likes?></p>
     <p class="txt"><?php echo nl2br($item->text)?></p>
 </div>
