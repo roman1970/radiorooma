777c66ac96caf1135470fe3fbd40cd636ae2f440
roman1970 <r0man4ernyshev@gmail.com>
2019-02-13 13:20:00 +0700

    adding pics to radio

--- views/site/room.php
+++ views/site/room.php
@@ -1,10 +1,19 @@
+<?php
+/**
+ * @var \app\models\RadioItem $item
+ */
+?>
 <div class="container" style="text-align: center">
     <h2><?=$item->title?></h2>
     <h5><p><?=$item->cat->name?></p></h5>
     <h3><p><?=$item->anons?></p></h3>
+    <?php if($item->audio) : ?>
     <audio controls>
         <source src="/uploads/<?=$item->audio?>">
     </audio>
+    <?php endif; ?>
     <p><a href="<?= \yii\helpers\Url::home();?>"> Радио </a>
+    <?php if($item->cat_id != 22) : ?>
     <p><?=nl2br($item->text)?></p>
+    <?php endif; ?>
 </div>
