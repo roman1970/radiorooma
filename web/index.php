8cb1013af40a44e4d9d9898e687096751e4b20b3
roman1970 <r0man4ernyshev@gmail.com>
2019-03-20 13:50:43 +0700

    coment debug

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 //echo '<h1 style="text-align: center; padding-top: 50px;">АНТРАКТ, НЕГОДЯИ!</h1>'; exit;
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
