079d16aa2fe1967073e527616c4c5349c65e57f2
roman1970 <r0man4ernyshev@gmail.com>
2019-05-19 09:04:42 +0700

    pic bard to item

--- views/site/room.php
+++ views/site/room.php
@@ -5,12 +5,12 @@
  */
 ?>
 <style>
-    img{
+    .pic{
         width: 100%;
     }
 </style>
 <div class="container" style="text-align: center">
-    <h2><?=$item->title?></h2>
+    <h2><img src='<?=\yii\helpers\Url::to('/img/barded2.png')?>' width="70px" id="player" class="bard_img"><span><?=$item->title?></span></h2>
     <h5><p><?=$item->cat->name?></p></h5>
     <h3><p><?=$item->anons?></p></h3>
     <?php if($item->audio) : ?>
@@ -20,7 +20,7 @@
     <?php endif; ?>
     <?php if($item->cat_id != 22) : ?>
         <p><?=nl2br($item->text)?></p>
-        <img src="<?=$pic->img ?>"/>
+        <img src="<?=$pic->img ?>" class="pic"/>
     <?php else: ?>
         <img src="<?=$item->img ?>"/>
     <?php endif; ?>
