eaffecaec54102ed6a299e15da841ca16578f89c
roman1970 <r0man4ernyshev@gmail.com>
2019-05-19 16:19:08 +0700

    item design radio

--- views/site/room.php
+++ views/site/room.php
@@ -8,11 +8,30 @@
     .pic{
         width: 80%;
     }
+    h2 img {
+        vertical-align: bottom;
+    }
+    .cat{
+        color: white;
+    }
+    .anons{
+        color: rgb(159, 173, 178);
+    }
+    .title{
+        color: rgb(244, 134, 104);
+    }
+    a {
+        color: rgb(117, 147, 180);
+        text-decoration: none;
+    }
 </style>
 <div class="container" style="text-align: center">
-    <h2><img src='<?=\yii\helpers\Url::to('/img/barded2.png')?>' width="70px" id="player" class="bard_img"><span><?=$item->title?></span></h2>
-    <h5><p><?=$item->cat->name?></p></h5>
-    <h3><p><?=$item->anons?></p></h3>
+    <h2>
+        <img src='<?=\yii\helpers\Url::to('/img/barded2.png')?>' width="70px" id="player" class="bard_img">
+        <span class="title"><?=$item->title?></span>
+    </h2>
+    <h5><p class="cat"><?=$item->cat->name?></p></h5>
+    <h3><p class="anons"><?=$item->anons?></p></h3>
     <?php if($item->audio) : ?>
     <audio controls controlsList="nodownload">
         <source src="/uploads/<?=$item->audio?>">
