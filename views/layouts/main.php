3b714b9f9eac6e1b924ccf3d6bdac8b7d5f4ca5c
roman1970 <r0man4ernyshev@gmail.com>
2019-05-31 08:31:37 +0700

    radiorooma window height

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1060,10 +1060,10 @@ AppAsset::register($this);
         //console.log(magnitola.clientHeight);
         if(img === null)
         //height = page.clientHeight;
-            height = window.innerHeight-magnitola.clientHeight - 100;
+            height = window.innerHeight-magnitola.clientHeight - 80;
         else
         //height = page.clientHeight + 200;
-            height = window.innerHeight-magnitola.clientHeight - 100;
+            height = window.innerHeight-magnitola.clientHeight - 80;
         var acc = document.getElementById('acc');
         var cats = document.getElementById('cats');
         //console.log(acc);
