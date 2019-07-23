25e5be0545283c19e95c91e49722aa6bc0359894
roman1970 <r0man4ernyshev@gmail.com>
2019-07-23 14:25:17 +0700

    misha new schema

--- views/item/item.php
+++ views/item/item.php
@@ -77,9 +77,12 @@
         acc.style.height = new_height + "px";
         cats.style.height = new_height + "px";
         if (img !== null && img.clientHeight > new_height) {
+            console.log(img.style.height);
             img.style.height = (img_height-100) + "px";
+            console.log(img.clientHeight);
+            console.log(img.style.height);
         }
-        console.log(img.clientHeight);
+
     });
         /*
 
