9ac028f865642d0c82c14d29a3373af880884b6c
roman1970 <r0man4ernyshev@gmail.com>
2019-11-02 08:46:10 +0700

    img add opacity

--- views/item/img.php
+++ views/item/img.php
@@ -3,7 +3,11 @@
         height: 80%;
         border-radius: 25px;
         max-height: 80%;
-        max-width: 100%
+        max-width: 100%;
+        background-color: rgba(119, 118, 103, 0.9);
+        opacity: 0.7;
+        /**/
+
     }
     .css-adaptive {
         max-width: 100%;
@@ -13,11 +17,22 @@
     .row{
         margin: 0;
     }
+    /*
+    .image-container {
+        position: relative;
+    }
+    .image-container {
+        position: absolute;
+        background-color: rgba(119, 118, 103, 0.5);
+    }
+    */
+     */
 </style>
-<div>
+<div class="image-container">
 <img src='<?=
-//\yii\helpers\Url::to('/img/poossusuddnnii-sshhkaff-s-knniiigigmaai_1559727712.jpg')
-$img->img?>'
+\yii\helpers\Url::to('/img/poossusuddnnii-sshhkaff-s-knniiigigmaai_1559727712.jpg')
+//$img->img
+?>'
      style="max-width: 100%;"
      id="img_rand"
      class="pic css-adaptive"/>
