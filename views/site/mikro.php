ae9538ef340d1eb35bed81c0d8b39b723407200c
roman1970 <r0man4ernyshev@gmail.com>
2020-06-05 17:24:44 +0700

    mikrotags test

--- views/site/mikro.php
+++ views/site/mikro.php
@@ -1,6 +1,6 @@
 
 <head>
-    <meta property="og:type" content="article" />
+    <meta property="og:type" content="music.song"/>
     <meta property="og:title" content="<?=$item->title?>">
     <meta property="og:url" content="http://radiorooma.noravbo.ru/<?=$item->alias?>.html">
     <meta property="og:description" name="description" content="<?=$item->anons?>">
