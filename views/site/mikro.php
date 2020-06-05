9b1e894849a7d12310d148b29af1c75707341c20
roman1970 <r0man4ernyshev@gmail.com>
2020-06-05 17:37:02 +0700

    mikrotags test

--- views/site/mikro.php
+++ views/site/mikro.php
@@ -4,6 +4,6 @@
     <meta property="og:title" content="<?=$item->title?>">
     <meta property="og:url" content="http://radiorooma.noravbo.ru/<?=$item->alias?>.html">
     <meta property="og:description" name="description" content="<?=$item->anons?>">
-    <meta property="og:image" content="http://radiorooma.noravbo.ru/<?=$pic->img ?>">
+    <meta property="og:image" content="http://static.noravbo.ru/<?=$pic->img ?>">
     <meta property="og:audio" content="http://radiorooma.noravbo.ru/uploads/<?=$item->audio?>" />
 </head>
