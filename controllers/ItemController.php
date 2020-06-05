cab03c10621bc1db57106fcbd5dd4ab756d3229d
roman1970 <r0man4ernyshev@gmail.com>
2020-06-05 16:23:58 +0700

    mikrotags test

--- controllers/ItemController.php
+++ controllers/ItemController.php
@@ -100,7 +100,7 @@ class ItemController extends Controller
 
         $pic = $pics[rand(0,count($pics)-1)];
 
-        return $this->render('/site/mikro', [
+        return $this->renderPartial('/site/mikro', [
             'item' => $item,
             'pic' => $pic,
             'kvn' => $kvn
